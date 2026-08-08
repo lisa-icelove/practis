e010f04b52ca142b87962c5d1c583817692a6d60
Lisa <lisa1105ice@gmail.com>
2026-08-08 12:45:43 +0900

    a

--- pages/2_Blood Type Personality Analysis.py
+++ pages/2_Blood Type Personality Analysis.py
@@ -5,8 +5,8 @@ st.write("※この診断で出力された結果はすべて事実無根です"
 st.write("以下の項目で当てはまったものにチェックを付けてください")
 st.write("---------------------------------------------")
 if "a" not in st.session_state:
-    st.session_state.a=""
+    st.session_state.a=0
 if st.checkbox("部屋は片付いているほうだ"):
-    st.session_state.a+=10
+    st.session_state+10
 
 st.write(f"{st.session_state.a}")
