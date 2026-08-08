774227c9b020a11d512064bf5cbe9ceb066370b8
Lisa <lisa1105ice@gmail.com>
2026-08-08 12:53:20 +0900

    A

--- pages/2_Blood Type Personality Analysis.py
+++ pages/2_Blood Type Personality Analysis.py
@@ -6,7 +6,17 @@ st.write("以下の項目で当てはまったものにチェックを付けて�
 st.write("---------------------------------------------")
 if "a" not in st.session_state:
     st.session_state.a=0
-if st.checkbox("部屋は片付いているほうだ"):
-    st.session_state += 10
+
+def add_a():
+    if st.session_state.a:
+        st.session_state.a += 10
+    else:
+        st.session_state.a -= 10
+
+st.checkbox(
+    "部屋は片付いているほうだ",
+    key="check",
+    on_change=add_a
+)
 
 st.write(f"{st.session_state.a}")
